e53fe44b72f731ddb7adf36424b6937cd26546ae
Ahmed-0sama <ahmedosama200266@yahoo.com>
2026-05-18 10:11:08 +0300

    Add project files.

---
+++ JwtCreator.Client/Class/TamperState.cs
@@ -0,0 +1,14 @@
+﻿namespace JwtStudio.Services;
+
+public class TamperState
+{
+    public string? Token { get; set; }
+    public string? Secret { get; set; }
+
+    public (string? token, string? secret) Consume()
+    {
+        var t = Token; var s = Secret;
+        Token = null; Secret = null;
+        return (t, s);
+    }
+}
---
+++ JwtCreator.Client/Program.cs
@@ -0,0 +1,12 @@
+using JwtCreator.Client;
+using JwtStudio.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<TamperState>();
+await builder.Build().RunAsync();
